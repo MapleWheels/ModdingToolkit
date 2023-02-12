d53f6132bd51437e4ec7ae32d2cbfcb2c46a144b
MapleWheels <njainanan@hotmail.com>
2023-02-12 05:11:30 -0500

    Setting up MSBuild custom tests.

---
+++ ProjectTasks/IncrementBuildTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ProjectTasks
+{
+    public class IncrementBuildTask : Task 
+    {
+        [Required]
+        public string SteamFileListPath { get; set; }
+        
+        public override bool Execute()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return true;
+        }
+    }
+}
